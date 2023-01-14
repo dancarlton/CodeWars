76260b5b41c034fe3e87acf3746d312058b736ac
Dan Carlton <danlouis.carlton@gmail.com>
2023-01-13 16:39:56 -0800

    daily codewars

--- 8kyu/Welcome!.js
+++ 8kyu/Welcome!.js
@@ -27,3 +27,26 @@
 // IP_ADDRESS_NOT_FOUND - ip address not in the database
 // IP_ADDRESS_REQUIRED - no ip address was supplied
 
+function greet(language) {
+  const greetings = {
+    english: 'Welcome',
+    czech: 'Vitejte',
+    danish: 'Velkomst',
+    dutch: 'Welkom',
+    estonian: 'Tere tulemast',
+    finnish: 'Tervetuloa',
+    flemish: 'Welgekomen',
+    french: 'Bienvenue',
+    german: 'Willkommen',
+    irish: 'Failte',
+    italian: 'Benvenuto',
+    latvian: 'Gaidits',
+    lithuanian: 'Laukiamas',
+    polish: 'Witamy',
+    spanish: 'Bienvenido',
+    swedish: 'Valkommen',
+    welsh: 'Croeso',
+  };
+
+  return greetings([language.toLowerCase()] || greetings.english);
+}
