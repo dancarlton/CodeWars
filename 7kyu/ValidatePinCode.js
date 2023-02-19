1339dc61e4e4158d6f8230dee5391825f5ec0e82
Dan Carlton <114847661+dancarlton@users.noreply.github.com>
2023-02-19 12:55:33 -0800

    Add files via upload

---
+++ 7kyu/ValidatePinCode.js
@@ -0,0 +1,34 @@
+// ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
+
+// If the function is passed a valid PIN string, return true, else return false.
+
+// Examples (Input --> Output)
+// "1234"   -->  true
+// "12345"  -->  false
+// "a234"   -->  false
+
+
+// First Try
+function validatePIN(pin) {
+    let length = 4
+    let typeOf = number
+    if (length === 4 & typeOf === number) {
+        return true
+    } else {
+      return false
+    }
+}
+
+// Second Try
+function validatePIN(pin) {
+    if (pin.length !== 4 && pin.length !== 6) {
+      return false;
+    }
+    for (let i = 0; i < pin.length; i++) {
+      if (isNaN(parseInt(pin[i]))) {
+        return false;
+      }
+    }
+    return true;
+  }
+  
