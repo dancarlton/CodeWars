55d45c509a8308ee52a01b13770ba9beeb42cb74
Dan Carlton <danlouis.carlton@gmail.com>
2023-10-22 15:37:19 -0700

    Mean of Array shortened

--- 8kyu/MeanOfArray.js
+++ 8kyu/MeanOfArray.js
@@ -12,42 +12,38 @@
 //avg value arr -> rounding down int
 
 // ATTEMPT 1
-function getAverage(marks) {
-  // add all numbers in array
-  let sum = 0
+// function getAverage(marks) {
+//   // add all numbers in array
+//   let sum = 0
 
-  // divide by amount of numbers in array by grabbing length
-  for (let i = 0; i < marks.length; i++) {
-    sum += marks[i]
-  }
+//   // divide by amount of numbers in array by grabbing length
+//   for (let i = 0; i < marks.length; i++) {
+//     sum += marks[i]
+//   }
 
-  //count amount of numbers in array
-  let count = marks.length
+//   //count amount of numbers in array
+//   let count = marks.length
 
-  // divide by the amount of numbers in array
-  let average = sum / count
+//   // divide by the amount of numbers in array
+//   let average = sum / count
 
-  // return average
-  return Math.floor(average)
-}
-const marks = [1, 2, 3, 4, 5]
-const results = getAverage.marks
+//   // return average
+//   return Math.floor(average)
+// }
+// const marks = [1, 2, 3, 4, 5]
+// const results = getAverage.marks
 
 // ATTEMPT 2
 
-function getAverage(arr) {
-  //loop arr -> adding to sum
-  let sum = arr.reduce((a, c) => a + c)
-  // avg div sum / total num of nums in arr
-  let avg = sum / arr.length
-  // round down
-  return Math.floor(avg)
-}
+// function getAverage(arr) {
+//   //loop arr -> adding to sum
+//   let sum = arr.reduce((a, c) => a + c)
+//   // avg div sum / total num of nums in arr
+//   let avg = sum / arr.length
+//   // round down
+//   return Math.floor(avg)
+// }
 
 // ATTEMPT 3
 
 const getAverage = arr => Math.floor(arr.reduce((a, c) => a + c) / arr.length)
-
-console.log(getAverage([2, 2, 2, 2]), 2)
-console.log(getAverage([1, 2, 3, 4, 5]), 3)
-console.log(getAverage([1, 1, 1, 1, 1, 1, 1, 2]), 1)
