22557638e071f92ff80b61e18f02ae5e7daca76b
Dan Carlton <danlouis.carlton@gmail.com>
2023-10-24 14:45:30 -0700

    Mean of Array

---
+++ 8kyu/MeanOfArray.js
@@ -0,0 +1,51 @@
+// It's the academic year's end, fateful moment of your school report. The averages must be calculated. All the students come to you and entreat you to calculate their average for them. Easy ! You just need to write a script.
+
+// Return the average of the given array rounded down to its nearest integer.
+
+// The array will never be empty.
+
+//getAverage([2,2,2,2]),2);
+//getAverage([1,2,3,4,5,]),3);
+//getAverage([1,1,1,1,1,1,1,2]),1);
+
+//arr -> never empty
+//avg value arr -> rounding down int
+
+// ATTEMPT 1
+function getAverage(marks) {
+  marks = [1, 2, 3, 4, 5]
+
+  // add all numbers in array
+  let sum = 0
+
+  // divide by amount of numbers in array by grabbing length
+  for (let i = 0; i < marks.length; i++) {
+    sum += marks[i]
+  }
+
+  let sumAll = marks.length
+
+  // divide by the amount of numbers in array
+
+  let average = {}
+}
+
+
+// ATTEMPT 2
+
+function getAverage(arr) {
+    //loop arr -> adding to sum
+    let sum = arr.reduce((a, c) => a + c)
+    // avg div sum / total num of nums in arr
+    let avg = sum / arr.length
+    // round down
+    return Math.floor(avg)
+}
+
+// ATTEMPT 3
+
+const getAverage = arr => Math.floor( arr.reduce((a,c) => a+c) / arr.length)
+
+console.log(getAverage([2, 2, 2, 2]), 2)
+console.log(getAverage([1, 2, 3, 4, 5]), 3)
+console.log(getAverage([1, 1, 1, 1, 1, 1, 1, 2]), 1)
